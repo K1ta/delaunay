7106e84ee70f106f9cc45d2ed91c5b46fa90ac54
Nikita Kuzmin <kobalt31@mail.ru>
2022-02-18 00:09:26 +0300

    remove todo

--- point.go
+++ point.go
@@ -8,7 +8,6 @@ type Point struct {
 }
 
 func (p *Point) isInTriangleCircumcircle(t Triangle) bool {
-	// TODO разобраться и привести формулу в порядок
 	var s = ((p.X-t.A.X)*(p.Y-t.C.Y)-(p.X-t.C.X)*(p.Y-t.A.Y))*((t.B.X-t.C.X)*(t.B.X-t.A.X)+(t.B.Y-t.C.Y)*(t.B.Y-t.A.Y)) +
 		((p.X-t.A.X)*(p.X-t.C.X)+(p.Y-t.A.Y)*(p.Y-t.C.Y))*((t.B.X-t.C.X)*(t.B.Y-t.A.Y)-(t.B.X-t.A.X)*(t.B.Y-t.C.Y))
 	return s > 0
